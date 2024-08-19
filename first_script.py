08e34cea60bba396d7369fa796b27a4fa283f367
Sami Askar <sami.askar@haw-hamburg.de>
2024-08-19 22:19:04 +0200

    fixed pylint issues

--- first_script.py
+++ first_script.py
@@ -1,11 +1,29 @@
+"""_summary_
+
+    Raises:
+        ValueError: _description_
+        ValueError: _description_
+        IOError: _description_
+"""
 import csv
 import os
 import sys
 import traceback
 
-def update_user_counter(csv_file, username):
+def update_user_counter(csv_file, _username):
+    """_summary_
+
+    Args:
+        csv_file (_type_): username, counter;
+        _username (_type_): _description_
+
+    Raises:
+        ValueError: _description_
+        ValueError: _description_
+        IOError: _description_
+    """
     try:
-        if not username.strip():
+        if not _username.strip():
             raise ValueError("Username cannot be empty.")
 
         user_found = False
@@ -13,7 +31,7 @@ def update_user_counter(csv_file, username):
         count =1
         if os.path.exists(csv_file):
             try:
-                with open(csv_file, mode='r', newline='') as file:
+                with open(csv_file, encoding='ascii', mode='r', newline='') as file:
                     reader = csv.reader(file)
                     rows = list(reader)
                     #print(f"Using CSV file: {csv_file}") #debugging line if csv doesnt "save"
@@ -22,7 +40,7 @@ def update_user_counter(csv_file, username):
                 if rows and rows[0] != ['user', 'counter']:
                     rows.insert(0, ['user', 'counter'])
                 for i, row in enumerate(rows):
-                    if row[0] == username:
+                    if row[0] == _username:
                         count = int(row[1]) + 1  # Increment the counter
                         rows[i][1] = str(count)
                         user_found = True
@@ -30,15 +48,14 @@ def update_user_counter(csv_file, username):
 
             except csv.Error as e:
                 raise ValueError(f"Error reading the CSV file: {e}")
-        
         if not user_found:
-            rows.append([username, '1'])
+            rows.append([_username, '1'])
 
         try:
-            with open(csv_file, mode='w', newline='') as file:
+            with open(csv_file, encoding='ascii', mode='w', newline='') as file:
                 writer = csv.writer(file)
                 writer.writerows(rows)
-                print(f"{username} has been first for {count} times.")
+                print(f"{_username} has been first for {count} times.")
         except IOError as e:
             raise IOError(f"Error writing to the CSV file: {e}")
 
@@ -58,6 +75,6 @@ if __name__ == "__main__":
     if len(sys.argv) < 1:
         print("Usage: python script.py <username>")
     else:
-        csv_file_path = 'user_data.csv'
+        CSV_FILE_PATH = 'user_data.csv'
         username = sys.argv[1]
-        update_user_counter(csv_file_path, username)
+        update_user_counter(CSV_FILE_PATH, username)
